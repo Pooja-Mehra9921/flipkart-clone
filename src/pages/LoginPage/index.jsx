c540ddd42a8f329cd7ebb977c2651d1fa15f1f00
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-01-10 18:09:21 +0530

    updateting code

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { Box, Paper } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import FLIPKART_IMAGE from "../../assents/images/flipkart-image.png";
 import CloseIcon from '@mui/icons-material/Close';
 
@@ -19,10 +19,14 @@ const LoginPage =()=>{
       <img style={{width:"100%", height:"100%"}} src={FLIPKART_IMAGE} alt="flipkart-image" />
     </Box>
     <Box className="right-flipkart-section">
-      <Box className="clone-icon">
+      <Box className="close-icon">
         <CloseIcon/>
       </Box>
-
+     
+     <Box className="text-container">
+      <Typography style={{fontWeight:"600"}} variant="body">Login</Typography>
+      <Typography variant="body1">Get access to your orders, whishlist and Recommendations</Typography>
+     </Box>
     </Box>
   </Paper>
 </Box>
